30691b773c3a5c93cf35487b2c6b3291b400213c
hoangtvps03510 <hoangtvps03510@fpt.edu.vn>
2016-11-16 20:20:54 +0700

    16.11

--- QuanLyThuVien/frmThongKe.vb
+++ QuanLyThuVien/frmThongKe.vb
@@ -1,34 +1,35 @@
-﻿Public Class frmThongKe 
+﻿Public Class frmThongKe
 
 
 
     Private Sub btnDoanhThu_Click(sender As Object, e As EventArgs) Handles btnDoanhThu.Click
+        frmDoanhThu.Close()
         frmDoanhThu.MdiParent = frmMainform
         frmDoanhThu.Show()
         frmDoanhThu.BringToFront()
     End Sub
 
     Private Sub btnchiphi_click(sender As Object, e As EventArgs) Handles btnChiPhi.Click
+        frmChiPhi.Close()
         frmChiPhi.MdiParent = frmMainform
         frmChiPhi.Show()
         frmChiPhi.BringToFront()
     End Sub
 
     Private Sub btnSach_click(sender As Object, e As EventArgs) Handles btnSach.Click
+        frmThongKeSach.Close()
         frmThongKeSach.MdiParent = frmMainform
         frmThongKeSach.Show()
         frmThongKeSach.BringToFront()
 
     End Sub
 
-
     Private Sub btnThanhVien_Click(sender As Object, e As EventArgs) Handles btnThanhVien.Click
+        frmThongKeTV.Close()
         frmThongKeTV.MdiParent = frmMainform
         frmThongKeTV.Show()
         frmThongKeTV.BringToFront()
     End Sub
-
-
 
     Private Sub btntonkho_Click(sender As Object, e As EventArgs) Handles btntonkho.Click
         frmThongKeTonKho1.MdiParent = frmMainform
